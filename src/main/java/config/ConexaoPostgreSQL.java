ee99a7fc898471edd50f894fde53c95d744abed5
Guimasz <gui10asouza@gmail.com>
2024-06-13 15:12:36 -0300

    Consertando jdbc

--- src/main/java/config/ConexaoPostgreSQL.java
+++ src/main/java/config/ConexaoPostgreSQL.java
@@ -6,11 +6,11 @@ import java.sql.SQLException;
 
 public class ConexaoPostgreSQL {
     // URL de conexão com o banco de dados
-    private static final String URL = "jdbc:postgresql://localhost:5432/nome_do_banco";
+    private static final String URL = "jdbc:postgresql://localhost:5432/universidade";
     // Nome de usuário do banco de dados
-    private static final String USUARIO = "usuario";
+    private static final String USUARIO = "postgres";
     // Senha do banco de dados
-    private static final String SENHA = "senha";
+    private static final String SENHA = "postgres";
 
     // Método para obter a conexão com o banco de dados
     public static Connection obterConexao() throws SQLException {
