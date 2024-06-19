aaf4c4349f9eba8d83c3e49aee82f028b194b778
Guimasz <gui10asouza@gmail.com>
2024-06-18 23:21:32 -0300

    ProfessorDisciplinaDao e ProfessorDisciplinaService

---
+++ src/main/java/Service/ProfessorDisciplinaService.java
@@ -0,0 +1,36 @@
+package service;
+
+import dao.ProfessorDisciplinaDao;
+import model.ProfessorDisciplina;
+import java.sql.SQLException;
+import java.util.List;
+
+public class ProfessorDisciplinaService {
+    private ProfessorDisciplinaDao professorDisciplinaDao;
+
+    public ProfessorDisciplinaService() {
+        professorDisciplinaDao = new ProfessorDisciplinaDao();
+    }
+
+    public void criarProfDis(int idProfessor, int idDisciplina) throws SQLException {
+        ProfessorDisciplina professorDisciplina = new ProfessorDisciplina(idDisciplina, idProfessor);
+        professorDisciplinaDao.criarProfDis(professorDisciplina);
+    }
+
+    public List<ProfessorDisciplina> findAll() throws SQLException {
+        return professorDisciplinaDao.findAll();
+    }
+
+    public List<ProfessorDisciplina> findByProfessor(int idProfessor) throws SQLException {
+        return professorDisciplinaDao.findByProfessor(idProfessor);
+    }
+
+    public void atualizarProfDis(int idProfessor, int idDisciplina, int newIdProfessor, int newIdDisciplina) throws SQLException {
+        ProfessorDisciplina professorDisciplina = new ProfessorDisciplina(newIdDisciplina, newIdProfessor);
+        professorDisciplinaDao.atualizarProfDis(professorDisciplina);
+    }
+
+    public void deletarProfDis(int idProfessor, int idDisciplina) throws SQLException {
+        professorDisciplinaDao.deletarProfDis(idProfessor, idDisciplina);
+    }
+}
---
+++ src/main/java/dao/ProfessorDisciplinaDao.java
@@ -0,0 +1,83 @@
+package dao;
+
+import config.ConexaoPostgreSQL;
+import model.ProfessorDisciplina;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProfessorDisciplinaDao {
+
+
+    public void criarProfDis(ProfessorDisciplina professorDisciplina) throws SQLException {
+        String sql = "INSERT INTO professorDisciplina (idProfessor, idDisciplina) VALUES (?, ?)";
+        try (Connection conexao = ConexaoPostgreSQL.obterConexao();
+             PreparedStatement stmt = conexao.prepareStatement(sql)) {
+
+
+            stmt.setInt(1, professorDisciplina.getIdProfessor());
+            stmt.setInt(2, professorDisciplina.getIdDisciplina());
+            stmt.executeUpdate();
+        }
+    }
+
+    public List<ProfessorDisciplina> findAll() throws SQLException {
+        List<ProfessorDisciplina> professorDisciplinas = new ArrayList<>();
+        String sql = "SELECT * FROM professorDisciplina";
+        try (Connection conexao = ConexaoPostgreSQL.obterConexao();
+             Statement stmt = conexao.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+            while (rs.next()) {
+                int idProfessor = rs.getInt("idProfessor");
+                int idDisciplina = rs.getInt("idDisciplina");
+                ProfessorDisciplina professorDisciplina = new ProfessorDisciplina(idDisciplina, idProfessor);
+                professorDisciplinas.add(professorDisciplina);
+            }
+        }
+        return professorDisciplinas;
+    }
+
+    public List<ProfessorDisciplina> findByProfessor(int idProfessor) throws SQLException {
+        List<ProfessorDisciplina> professorDisciplinas = new ArrayList<>();
+        String sql = "SELECT * FROM ProfessorDisciplina WHERE idProfessor = ?";
+        try (Connection conexao = ConexaoPostgreSQL.obterConexao();
+             PreparedStatement stmt = conexao.prepareStatement(sql)) {
+            stmt.setInt(1, idProfessor);
+            try (ResultSet rs = stmt.executeQuery()) {
+                while (rs.next()) {
+                    int idDisciplina = rs.getInt("idDisciplina");
+                    ProfessorDisciplina professorDisciplina = new ProfessorDisciplina(idDisciplina, idProfessor);
+                    professorDisciplinas.add(professorDisciplina);
+                }
+            }
+        }
+        return professorDisciplinas;
+    }
+
+    public void atualizarProfDis(ProfessorDisciplina professorDisciplina) throws SQLException {
+        String sql = "UPDATE ProfessorDisciplina SET idProfessor = ?, idDisciplina = ? WHERE idProfessor = ? AND idDisciplina = ?";
+        try (Connection conexao = ConexaoPostgreSQL.obterConexao();
+             PreparedStatement stmt = conexao.prepareStatement(sql)) {
+            stmt.setInt(1, professorDisciplina.getIdProfessor());
+            stmt.setInt(2, professorDisciplina.getIdDisciplina());
+            stmt.setInt(3, professorDisciplina.getIdProfessor());
+            stmt.setInt(4, professorDisciplina.getIdDisciplina());
+            stmt.executeUpdate();
+        }
+    }
+
+    public void deletarProfDis(int idProfessor, int idDisciplina) throws SQLException {
+        String sql = "DELETE FROM ProfessorDisciplina WHERE idProfessor = ? AND idDisciplina = ?";
+        try (Connection conexao = ConexaoPostgreSQL.obterConexao();
+             PreparedStatement stmt = conexao.prepareStatement(sql)) {
+            stmt.setInt(1, idProfessor);
+            stmt.setInt(2, idDisciplina);
+            stmt.executeUpdate();
+        }
+    }
+}
+
