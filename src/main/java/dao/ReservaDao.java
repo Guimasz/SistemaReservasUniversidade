19b7a6daadee5fa683121cfde0827a82a60ddb99
Guimasz <gui10asouza@gmail.com>
2024-06-19 00:04:38 -0300

    Consertando ReservaDao

--- src/main/java/dao/ReservaDao.java
+++ src/main/java/dao/ReservaDao.java
@@ -14,15 +14,15 @@ public class ReservaDao {
 
     // Método para recuperar todas as reservas
     public ArrayList<Reserva> findAll() {
-        String sql = "SELECT r.id, r.data_hora, r.tempo, r.aprovada, " +
-                "l.id AS laboratorio_id, l.descricao AS laboratorio_descricao, l.capacidade AS laboratorio_capacidade, l.status AS laboratorio_status, " +
-                "p.id AS professor_id, p.nome AS professor_nome, p.status AS professor_status, " +
-                "t.id AS turma_id, d.id AS disciplina_id, d.sigla AS disciplina_sigla, d.descricao AS disciplina_descricao, d.status AS disciplina_status " +
+        String sql = "SELECT r.id, r.datahora, r.duracao, r.aprovada, " +
+                "l.id AS lab_id, l.descricao AS lab_desc, l.capacidade AS lab_cap, l.status AS lab_status, " +
+                "p.id AS prof_id, p.nome AS prof_nome, p.status AS prof_status, " +
+                "t.id AS turma_id, d.id AS disc_id, d.sigla AS disc_sigla, d.descricao AS disc_desc, d.status AS disc_status " +
                 "FROM reserva r " +
-                "INNER JOIN laboratorio l ON r.laboratorio_id = l.id " +
-                "INNER JOIN professor p ON r.professor_id = p.id " +
-                "INNER JOIN turma t ON r.turma_id = t.id " +
-                "INNER JOIN disciplina d ON t.disciplina_id = d.id";
+                "INNER JOIN laboratorio l ON r.laboratorio = l.id " +
+                "INNER JOIN professor p ON r.professor = p.id " +
+                "INNER JOIN turma t ON r.turma = t.id " +
+                "INNER JOIN disciplina d ON t.disciplina = d.id";
         ArrayList<Reserva> reservas = new ArrayList<>();
 
         try (Connection conexao = ConexaoPostgreSQL.obterConexao();
@@ -32,30 +32,30 @@ public class ReservaDao {
             while (rs.next()) {
                 Reserva reserva = new Reserva();
                 reserva.setId(rs.getInt("id"));
-                reserva.setDataHora(rs.getTimestamp("data_hora").toLocalDateTime());
-                reserva.setTempo(Duration.ofMinutes(rs.getLong("tempo")));
+                reserva.setDataHora(rs.getTimestamp("datahora").toLocalDateTime());
+                reserva.setTempo(Duration.ofMinutes(rs.getLong("duracao")));
                 reserva.setAprovada(rs.getBoolean("aprovada"));
 
                 Laboratorio laboratorio = new Laboratorio();
-                laboratorio.setId(rs.getInt("laboratorio_id"));
-                laboratorio.setDescricao(rs.getString("laboratorio_descricao"));
-                laboratorio.setCapacidade(rs.getInt("laboratorio_capacidade"));
-                laboratorio.setStatus(rs.getBoolean("laboratorio_status"));
+                laboratorio.setId(rs.getInt("lab_id"));
+                laboratorio.setDescricao(rs.getString("lab_desc"));
+                laboratorio.setCapacidade(rs.getInt("lab_cap"));
+                laboratorio.setStatus(rs.getBoolean("lab_status"));
                 reserva.setLaboratorio(laboratorio);
 
                 Professor professor = new Professor();
-                professor.setId(rs.getInt("professor_id"));
-                professor.setNome(rs.getString("professor_nome"));
-                professor.setStatus(rs.getBoolean("professor_status"));
+                professor.setId(rs.getInt("prof_id"));
+                professor.setNome(rs.getString("prof_nome"));
+                professor.setStatus(rs.getBoolean("prof_status"));
                 reserva.setProfessor(professor);
 
                 Turma turma = new Turma();
                 turma.setId(rs.getInt("turma_id"));
                 Disciplina disciplina = new Disciplina();
-                disciplina.setId(rs.getInt("disciplina_id"));
-                disciplina.setSigla(rs.getString("disciplina_sigla"));
-                disciplina.setDescricao(rs.getString("disciplina_descricao"));
-                disciplina.setStatus(rs.getBoolean("disciplina_status"));
+                disciplina.setId(rs.getInt("disc_id"));
+                disciplina.setSigla(rs.getString("disc_sigla"));
+                disciplina.setDescricao(rs.getString("disc_desc"));
+                disciplina.setStatus(rs.getBoolean("disc_status"));
                 turma.setDisciplina(disciplina);
                 reserva.setTurma(turma);
 
@@ -71,9 +71,9 @@ public class ReservaDao {
     // Método para recuperar uma reserva pelo ID
     public Reserva findReservaById(int id) {
         String sql = "SELECT r.id, r.datahora, r.duracao, r.aprovada, " +
-                "l.id AS laboratorio_id, l.descricao AS laboratorio_descricao, l.capacidade AS laboratorio_capacidade, l.status AS laboratorio_status, " +
-                "p.id AS professor_id, p.nome AS professor_nome, p.status AS professor_status, " +
-                "t.id AS turma_id, d.id AS disciplina_id, d.sigla AS disciplina_sigla, d.descricao AS disciplina_descricao, d.status AS disciplina_status " +
+                "l.id AS lab_id, l.descricao AS lab_desc, l.capacidade AS lab_cap, l.status AS lab_status, " +
+                "p.id AS prof_id, p.nome AS prof_nome, p.status AS prof_status, " +
+                "t.id AS turma_id, d.id AS disc_id, d.sigla AS disc_sigla, d.descricao AS disc_desc, d.status AS disc_status " +
                 "FROM reserva r " +
                 "INNER JOIN laboratorio l ON r.laboratorio = l.id " +
                 "INNER JOIN professor p ON r.professor = p.id " +
@@ -94,26 +94,27 @@ public class ReservaDao {
                     reserva.setDataHora(rs.getTimestamp("datahora").toLocalDateTime());
                     reserva.setTempo(Duration.ofMinutes(rs.getLong("duracao")));
                     reserva.setAprovada(rs.getBoolean("aprovada"));
+
                     Laboratorio laboratorio = new Laboratorio();
-                    laboratorio.setId(rs.getInt("laboratorio_id"));
-                    laboratorio.setDescricao(rs.getString("laboratorio_descricao"));
-                    laboratorio.setCapacidade(rs.getInt("laboratorio_capacidade"));
-                    laboratorio.setStatus(rs.getBoolean("laboratorio_status"));
+                    laboratorio.setId(rs.getInt("lab_id"));
+                    laboratorio.setDescricao(rs.getString("lab_desc"));
+                    laboratorio.setCapacidade(rs.getInt("lab_cap"));
+                    laboratorio.setStatus(rs.getBoolean("lab_status"));
                     reserva.setLaboratorio(laboratorio);
 
                     Professor professor = new Professor();
-                    professor.setId(rs.getInt("professor_id"));
-                    professor.setNome(rs.getString("professor_nome"));
-                    professor.setStatus(rs.getBoolean("professor_status"));
+                    professor.setId(rs.getInt("prof_id"));
+                    professor.setNome(rs.getString("prof_nome"));
+                    professor.setStatus(rs.getBoolean("prof_status"));
                     reserva.setProfessor(professor);
 
                     Turma turma = new Turma();
                     turma.setId(rs.getInt("turma_id"));
                     Disciplina disciplina = new Disciplina();
-                    disciplina.setId(rs.getInt("disciplina_id"));
-                    disciplina.setSigla(rs.getString("disciplina_sigla"));
-                    disciplina.setDescricao(rs.getString("disciplina_descricao"));
-                    disciplina.setStatus(rs.getBoolean("disciplina_status"));
+                    disciplina.setId(rs.getInt("disc_id"));
+                    disciplina.setSigla(rs.getString("disc_sigla"));
+                    disciplina.setDescricao(rs.getString("disc_desc"));
+                    disciplina.setStatus(rs.getBoolean("disc_status"));
                     turma.setDisciplina(disciplina);
                     reserva.setTurma(turma);
                 }
@@ -156,7 +157,7 @@ public class ReservaDao {
 
     // Método para atualizar uma reserva existente
     public void atualizarReserva(Integer id, Reserva reserva) {
-        String sql = "UPDATE reserva SET laboratorio_id = ?, professor_id = ?, turma_id = ?, data_hora = ?, tempo = ?, aprovada = ? WHERE id = ?";
+        String sql = "UPDATE reserva SET laboratorio = ?, professor = ?, turma = ?, datahora = ?, duracao = ?, aprovada = ? WHERE id = ?";
 
         try (Connection conexao = ConexaoPostgreSQL.obterConexao();
              PreparedStatement stmt = conexao.prepareStatement(sql)) {
